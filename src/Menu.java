47531f2a6f2c3b8cd95866682b6f4573fe0e366f
GRB2004 <117335824+GRB2004@users.noreply.github.com>
2023-11-25 16:21:08 -0600

    Llame a la funciones nuevas que faltaban al árbol.

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,5 @@
 import java.time.LocalDate;
-
+import java.util.Scanner;
 public class Menu {
     AVL arbolPrincipal = new AVL();
     public void insertarPaciente(){
@@ -16,22 +16,28 @@ public class Menu {
 
     }
     public void buscar(){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("De el NSS del paciente que quiere buscar");
+        int nss = scanner.nextInt();
+        arbolPrincipal.buscar(nss);
     }
     public void mostrarPreorden(){
-
+        arbolPrincipal.mostrarPreorden();
     }
     public void mostrarInorden(){
-
+        arbolPrincipal.mostrarInorden();
     }
     public void mostrarPostorden(){
-
+         arbolPrincipal.mostrarPostorden();
     }
     public void exportar(){
 
     }
     public void eliminar(){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("De el NSS del paciente que quiere eliminar");
+        int nss = scanner.nextInt();
+        arbolPrincipal.eliminar(arbolPrincipal.raiz,nss);
     }
     public void salir(){
 
