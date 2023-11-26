1c961b81da546abaeee488549dfa028b28ea7b29
GRB2004 <117335824+GRB2004@users.noreply.github.com>
2023-11-25 20:15:14 -0600

    Agregue función ingresar cita

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,6 +1,5 @@
+import java.util.Scanner;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 public class Paciente {
     public int NSS;
     public String nombres;
@@ -17,13 +16,39 @@ public class Paciente {
         this.apellidoMaterno = apellidoMaterno;
         this.sexo = sexo;
         this.edad = edad;
-        citas = new ListaLigada();
+        this.citas = new ListaLigada();
     }
     public Paciente() {
         citas = new ListaLigada();
     }
-    public void agregarCita(LocalDateTime fecha, String nombreMedico, int consultorio) {
-        Citas nuevaCita = new Citas(fecha, nombreMedico, consultorio);
-        citas.insertarOrdenado(nuevaCita);
+    
+    public void mostrarPaciente(){
+        System.out.println("NSS: " + NSS);
+        System.out.println("Nombres: " + nombres);
+        System.out.println("Apellido Paterno: " + apellidoPaterno);
+        System.out.println("Apellido Materno: " + apellidoMaterno);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Edad: " + edad);
+        System.out.println("Citas:");
     }
+    
+    public void insertarCita(){
+        Scanner scanner = new Scanner(System.in);
+        
+             String fechastr = "";
+             String nombreMedico ="";
+             int consultorio = 0;
+             System.out.println("Ingrese la fecha");
+             fechastr = scanner.nextLine();
+             LocalDate fecha = LocalDate.parse(fechastr);
+             System.out.println("Ingrese el Médico");
+             nombreMedico = scanner.next();
+             System.out.println("Ingrese el Consultorio");
+             consultorio = scanner.nextInt();
+             Citas nuevaCita = new Citas(fecha, nombreMedico, consultorio);
+             this.citas.insertarOrdenado(nuevaCita);
+            
+    }
+    
+    
 }
